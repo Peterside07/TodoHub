b8c76bea5200adaac5340169ebc05fc3fe3ebf13
Peter Ipoola <peteripoola@yahoo.com>
2023-07-26 09:47:21 -0700

    Add reusable widgets

---
+++ lib/common/widgets/height_spacer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HeightSpacer extends StatelessWidget {
+  const HeightSpacer({
+    Key? key,
+    required this.height,
+  }) : super(key: key);
+
+  final double height;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(height: height,);
+  }
+}
---
+++ lib/common/widgets/reusable_text.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ReusableText extends StatelessWidget {
+  const ReusableText({
+    Key? key,
+    required this.text,
+    this.style,
+  }) : super(key: key);
+
+  final String text; 
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Text(text,
+    maxLines: 1,
+    textAlign: TextAlign.left,
+    softWrap: true,
+    overflow: TextOverflow.fade,
+    style: style,
+    );
+  }
+}
