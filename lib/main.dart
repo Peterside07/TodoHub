b438c10de47b66e0570d476205afa43b90b102fb
Peter Ipoola <peteripoola@yahoo.com>
2023-07-26 08:58:11 -0700

    Add `hooks_riverpod` package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'features/todo/pages/homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -13,23 +18,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      useInheritedMediaQuery: true,
-      designSize: const Size(375, 825),
-      minTextAdapt: true,
-      builder: (context, child) {
-        return MaterialApp(
-          title: 'Todo Hub',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-            useMaterial3: true,
-          ),
-          home: const HomePage(),
-        );
-      }
-    );
+        useInheritedMediaQuery: true,
+        designSize: const Size(375, 825),
+        minTextAdapt: true,
+        builder: (context, child) {
+          return MaterialApp(
+            title: 'Todo Hub',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const HomePage(),
+          );
+        });
   }
 }
-
-
-
